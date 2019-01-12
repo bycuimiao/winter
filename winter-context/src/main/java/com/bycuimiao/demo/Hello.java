fc2d3b3234d7a48a32807cba5d964d9020c1a9c3
miao.cui <miao.cui@bianlifeng.com>
2019-01-12 12:16:28 +0800

    hello

---
+++ winter-context/src/main/java/com/bycuimiao/demo/Hello.java
@@ -0,0 +1,13 @@
+package com.bycuimiao.demo;
+
+/**
+ * hello winter.
+ *
+ * @author cuimiao
+ * @since 12 January 2019
+ */
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello winter");
+	}
+}
